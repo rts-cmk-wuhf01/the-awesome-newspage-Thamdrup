0d1c4ddb1644b363a571f89f86f7fbc51c79ef33
Rasmus Thamdrup <5579327@rts-365.dk>
2019-06-04 13:34:59 +0200

    Dynamic team members

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -34,7 +34,7 @@ module.exports = (app) => {
 
    app.get('/single-post', (req, res, next) => {
       res.render('single-post',{
-      'dateTest':"2019-07-16 14:19",
+         'dateTest':"2019-07-16 14:19",
          'dateTest2':"2019-06-10 13:42",
          'dateTest3':"2019-01-01 11:11",
          'dateTest4':"1991-03-19 19:16",
@@ -43,7 +43,52 @@ module.exports = (app) => {
    });
 
    app.get('/about', (req, res, next) => {
-      res.render('about');
+
+      let employees = [
+         {
+            "name":"Employee 101",
+            "position":"Editor",
+            "imgSrc":"img/bg-img/t7.jpg"
+         },
+         {
+            "name":"Employee 102",
+            "position":"Super Editor",
+            "imgSrc":"img/bg-img/t2.jpg"
+         },
+         {
+            "name":"Employee 103",
+            "position":"Ultra Editor",
+            "imgSrc":"img/bg-img/t3.jpg"
+         },
+         {
+            "name":"Employee 104",
+            "position":"Omega Editor",
+            "imgSrc":"img/bg-img/t4.jpg"
+         },
+         {
+            "name":"Employee 105",
+            "position":"Senior Editor",
+            "imgSrc":"img/bg-img/t5.jpg"
+         },
+         {
+            "name":"Employee 106",
+            "position":"Super Ultra Editor",
+            "imgSrc":"img/bg-img/t6.jpg"
+         },
+         {
+            "name":"Employee 107",
+            "position":"Super Ultra Omega Editor",
+            "imgSrc":"img/bg-img/t1.jpg"
+         },
+         {
+            "name":"Employee 108",
+            "position":"Super Ultra Omega Senior Editor",
+            "imgSrc":"img/bg-img/t8.jpg"
+         },
+      ]
+      res.render('about',{
+         "currentEmployees":employees
+      });
    });
 
 
