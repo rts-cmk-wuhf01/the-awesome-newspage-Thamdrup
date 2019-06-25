c887da5ad25832b981cd76b61ca5966f3074489f
Thamdrup <43438980+Thamdrup@users.noreply.github.com>
2019-06-25 14:56:22 +0200

    categories udskrevet

--- server/routes/admin-routes.js
+++ server/routes/admin-routes.js
@@ -2,17 +2,30 @@ const mysql = require('../config/mysql');
 module.exports = (app) => {
 
     app.get("/admin-categories", async (req, res, next) => {
-        // her skal alle kategorier hentes og sendes til template filen.....
+        // her skal alle kategorier hentes og sendes til template filen....
+        let db = await mysql.connect();
+        let [categories] = await db.execute('SELECT * FROM categories');
+        db.end();
         res.render('admin-categories',{
-
+            "categories": categories
         })
       });
     
       app.get("/admin-categories/edit", async (req, res, next) => {
         // her skal alle kategorier hentes og sendes til template filen.....
+
+
         res.send('whateva',{
 
         })
+      });
+
+
+
+
+      app.post("/admin-categories", async (req, res, next) => {
+        // her skal vi modtage form data og indsætte det i databasen
+        // send bruger tilbage til kategori admin listen
       });
 
 }
